8db24ba7a83016ac602205fe6b4a2807f036e803
eastcoastsb <ryanmcgovern@Ryans-MacBook-Air.local>
2015-07-16 13:37:29 -0400

    new render json

--- app/controllers/athlete_controller.rb
+++ app/controllers/athlete_controller.rb
@@ -1,4 +1,10 @@
 class AthleteController < ApplicationController
   def index
+    @athlete = Athlete.all
+    render json: @athlete
+    # Use this if you need more than one format i.e. html and xml
+    # respond_to do |format|
+    #   format.json { render json: @athlete }
+    # end
   end
 end
